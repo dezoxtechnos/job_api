5fdff82db5cc87ff890523f4fe7b5335ed19606a
Jaseel PM <jaseel@Jaseel-Mac.local>
2025-10-24 22:17:21 +0530

    Updated config

--- config.py
+++ config.py
@@ -2,19 +2,13 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "change_me")
-    SQLALCHEMY_DATABASE_URI = "postgresql://jaseel@localhost:5432/post"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # SQLALCHEMY_DATABASE_URI = "sqlite:////Users/jaseel/Desktop/AI105/main/database.db"
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
-    REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+    REDIS_URL = os.getenv("REDIS_URL", "")
     MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY", "")
-    # OPENROUTER_API_KEY="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"
-    OPENROUTER_API_KEY="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"
-    X_API_KEY='testtoken123'
+    OPENROUTER_API_KEY=os.getenv("OPENROUTER_API_KEY", "")
+    X_API_KEY=os.getenv("X_API_KEY", "")
     FAST_SLOTS = int(os.getenv("FAST_SLOTS", 2))
     SLOW_SLOTS = int(os.getenv("SLOW_SLOTS", 2))
 
-
 config = Config()
-
-#  postgresql://jaseel@localhost:5432/post postgresql://jobsuser@localhost:5432/jobsdb
